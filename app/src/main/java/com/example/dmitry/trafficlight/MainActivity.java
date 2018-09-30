a76221b0afbbac006fa1533056cbe558062715b4
Trushenkov Dmitry <diman28051999@gmail.com>
2018-09-30 19:21:05 +0300

    Class became implements View.onClickListener

--- app/src/main/java/com/example/dmitry/trafficlight/MainActivity.java
+++ app/src/main/java/com/example/dmitry/trafficlight/MainActivity.java
@@ -8,41 +8,39 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements  View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button red;
     private Button yellow;
     private Button green;
     private View view;
+    private int currentBackgroundColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Button-элементы
         red = findViewById(R.id.red_button);
         yellow = findViewById(R.id.yellow_button);
         green = findViewById(R.id.green_button);
         view = findViewById(R.id.activity);
 
-
         red.setOnClickListener(this);
         yellow.setOnClickListener(this);
         green.setOnClickListener(this);
-
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
 
-        int color = Color.TRANSPARENT;
+        currentBackgroundColor = Color.TRANSPARENT;
         Drawable background = view.getBackground();
         if (background instanceof ColorDrawable)
-            color = ((ColorDrawable) background).getColor();
+            currentBackgroundColor = ((ColorDrawable) background).getColor();
 
-        outState.putInt("background_color", color);
+        outState.putInt("background_color", currentBackgroundColor);
     }
 
     @Override
@@ -57,15 +55,12 @@ public class MainActivity extends AppCompatActivity implements  View.OnClickList
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            // кнопка "Красный"
             case R.id.red_button:
                 view.setBackgroundColor(Color.RED);
                 break;
-            // кнопка "Желтый"
             case R.id.yellow_button:
                 view.setBackgroundColor(Color.YELLOW);
                 break;
-            //кнопка "Зеленый"
             case R.id.green_button:
                 view.setBackgroundColor(Color.GREEN);
                 break;
